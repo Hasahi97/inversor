c5589be8635e55f1b7e4cc83d53cb23d35135fd8
Henrique Shinji Yamada Asahi <henriques.asahi@gmail.com>
2020-03-16 16:53:30 -0400

    Codigo funcional

--- src/main.c
+++ src/main.c
@@ -1,14 +1,75 @@
 /* Data de submissao:
- * Nome:
- * RA:
+ * Nome: Henrique Shinji Yamada Asahi
+ * RA: 217832
  */
 
 #include <stdio.h>
 const int tam_buffer=100;
 
-int main() {
-  char buffer[tam_buffer];
-  fgets(buffer, tam_buffer, stdin);
-  printf("%s", buffer);
-  return 0;
+typedef struct {
+  unsigned char dados[100];
+  int length;
+} stack, *Stack;
+
+void stack_init(Stack sta)
+{
+    sta->length = 0;
+}
+
+void push(Stack sta, char c)
+{
+	if(sta->length < 100)
+	{
+		sta->dados[sta->length] = c;
+		sta->length++;
+	}
+}
+
+char pop(Stack sta)
+{
+	unsigned char c;
+	if(sta->length > 0)
+	{
+		sta->length--;
+		c = sta->dados[sta->length];
+	}
+	return c;
+}
+
+int main() 
+{
+    char buffer[tam_buffer];
+	stack pilha;
+	unsigned int kBuffer, k;
+	stack_init(&pilha);
+	
+	kBuffer = 0;
+	
+    fgets(buffer, tam_buffer, stdin);
+    
+    while(buffer[kBuffer] != '\n')
+    {
+        kBuffer++;
+    }
+    
+    for(k = 0; k <= kBuffer; k++)
+    {
+        if(buffer[k] == ' ' || k == kBuffer)
+        {
+            while(pilha.length > 0)
+            {
+                printf("%c", pop(&pilha));
+            }
+            if(buffer[k] == ' ')
+            {
+				printf(" ");
+			}
+        }
+        else
+        {
+            push(&pilha, buffer[k]);
+        }
+    }
+    printf("\n");
+    return 0;
 }
